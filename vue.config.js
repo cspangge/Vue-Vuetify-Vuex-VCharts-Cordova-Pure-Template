da78cf004662df5a3b82c347473320f01ab5beac
pangge <cspangge@users.noreply.github.com>
2020-04-03 08:45:07 +1100

    Optimize Vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-    outputDir: "www",
-    publicPath: "",
-};
+  "outputDir": "www",
+  "publicPath": "",
+  "lintOnSave": true,
+  "productionSourceMap": process.env.NODE_ENV === 'development',
+
+  "transpileDependencies": [
+    "vuetify"
+  ],
+
+  "configureWebpack": {
+    "performance": {
+      "hints": process.env.NODE_ENV === 'production' ? "warning" : false,
+      "maxEntrypointSize": 512000,
+      "maxAssetSize": 512000,
+    },
+  },
+}
